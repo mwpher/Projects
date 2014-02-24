2fb75d0a2dd8d6c95d7b0d1de8b3979d0c20ee63
Matthew Pherigo <hybrid120@gmail.com>
2014-02-24 11:51:47 -0600

    Finished helloworld

---
+++ hello-world.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello-world.c
+ *
+ *    Description:  Hello world
+ *
+ *        Version:  1.0
+ *        Created:  02/24/14 11:18:42
+ *       Revision:  none
+ *       Compiler:  clang
+ *
+ *         Author:  Matthew  Pherigo (mwp), hybrid120@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello, world!");
+	return 0;
+}
+
