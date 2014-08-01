f70d9d439da6b0c2ed4cdf6855708a04ebc7b9f0
Matthew Pherigo <hybrid120@gmail.com>
2014-07-31 22:41:06 -0500

    add primefac.c, update(break) gnirts.c

---
+++ Numbers/primefac.c
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  primefac.c
+ *
+ *    Description:  
+ *
+ *        Version:  0.1
+ *        Created:  07/05/14 22:30:54
+ *       Revision:  none
+ *       Compiler:  Clang
+ *
+ *         Author:  Matthew Pherigo (mwp), hybrid120@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+
--- Text/gnirts.c
+++ Text/gnirts.c
@@ -24,20 +24,39 @@
 #include <stdbool.h>
 #include <string.h>
 
-int main(void) {
+int main(int argc, char* argv[]) {
     unsigned int len = 0;
     unsigned int max_len = 141;
     unsigned int n = 0;
+    unsigned int spacecounter = 0;
     char buffer[max_len];
     char* gnirts = NULL;
-    char* string = getString(max_len - 1);
+    char* string = NULL;
+
+    if (argc > 1) {
+        argc--;
+
+        if (strcmp(argv[1], "-l") == 0) {
+            max_len = (unsigned int)stringtoint(argv[2]);
+            if(errno != 0)
+            { ERROR; exit(2); }
+        }
+        else {
+            for (int i = 1; i < argc; i++) {
+                strlcat(buffer + (spacecounter * sizeof(char)), argv[i], max_len);
+                spacecounter = spacecounter + (unsigned int)sprintf(argv[i], NULL);
+            }
+        }
+    }
+
+    string = getString(max_len - 1);
     if (string == NULL)
     { ERROR; exit(1); }
 
     len = strnlen(string, max_len);
     
     buffer[len + 1] = '\0';
-    len--;  /* decrease len so that things like for(n<len) make sense */
+    len--;  /* decrease len so that things like for(n <= len) make sense */
     for (n = 0; n <= len; n++) {
         buffer[len - n] = string[n];
         if (buffer[len - n] == '\0')
