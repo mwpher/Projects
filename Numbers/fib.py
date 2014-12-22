b2695a003f4c7d818352a418899ea4b4a20be59c
Matthew Pherigo <hybrid120@gmail.com>
2014-12-21 22:19:51 -0600

    improve fib.py

--- Numbers/fib.py
+++ Numbers/fib.py
@@ -1,36 +1,45 @@
 # fib.py
+
 import sys
 
 class fib:
-	result = [1, 1]
-	iterations = len(result) - 1
-	
+	d = dict()
+	counter = 1
 	def __init__(self, n=0):
-		print "#0 is", str(self.result[0])
-		self.printlast()
+		self.d = {'result': [1, 1], 'iterations': 2}
 		if n == 0:
+			self.printresults()
 			while True:
 				self.iter()
-				self.printlast()
-				answer = raw_input('Continue? (Y/n) ->')
-				if (len(answer) > 0) and answer[0] == ('N' or 'n'):
+				self.printlatest()
+				answer = raw_input("Continue? (Y/n) ->").lower()
+				if len(answer) > 0 and answer[0] == 'n':
 					break
 				else:
 					continue
+			print "Done! \n\n"
+			self.printresults()
 		else:
-			while self.iterations < n:
-				self.iter()
-				self.printlast()
-
-	def iter(self):
-		self.result.append(self.result[self.iterations - 1] + self.result[self.iterations])
-		self.iterations += 1
-	def printlast(self):
-		print "#" + str(self.iterations), "is", str(self.result[self.iterations])
+			self.iter(n - 2)
+			self.printresults()
+	
+	def printresults(self):
+		i = 0
+		while i < len(self.d['result']):
+			print "#" + str(i + 1), "is", str(self.d['result'][i])
+			i += 1
+	def printlatest(self):
+		print "#" + str(self.counter + 1), "is", str(self.d['result'][self.counter])
+	def iter(self, n=1):
+		i = 0
+		while i < n:
+			nextone = self.d['result'][self.counter] + self.d['result'][(self.counter - 1)]
+			self.d['result'].append(nextone)
+			self.d['iterations'] += 1
+			self.counter += 1
+			i += 1
 
 if len(sys.argv) > 1:
 	f = fib(int(sys.argv[1]))
 else:
 	f = fib()
-
-print "iterations: ", f.iterations, "\n", "result: ", f.result
