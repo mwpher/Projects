e31c424828c6b304ca7380d4a68bfa30f414db40
Matthew Pherigo <hybrid120@gmail.com>
2014-12-23 19:48:58 -0600

    add file-output functionality and remove class

--- Numbers/nextprime.py
+++ Numbers/nextprime.py
@@ -1,46 +1,64 @@
 # nextprime.py
 import sys
 
-class nextprime:
-	primes = [1, 2, 3]
-	numofprimes = len(primes)
-	def __init__(self, n=0):
-		if n == 0:
-			while True:
-				self.nextprime()
-				answer = raw_input("Continue? (Y/n)")
-				if len(answer) > 0 and answer[0].lower() == 'n':
-					break
-		else:
-			while self.numofprimes < n:
-				self.nextprime()
-		print "I found", str(self.numofprimes) + ":", str(self.primes)
-	def nextprime(self):
-		prime = False
-		n = self.primes[len(self.primes) - 1] + 1
-		while True:
-			factor = self.isprime(n)
-			if factor == 0:
-				self.primes.append(n)
-				break
-			else:
-				print str(n), "is divisible by", str(factor)
-				n += 1
-		print str(n), "is prime"
-		self.numofprimes += 1
-	def isprime(self, n):
-		i = 2
-		while i < n:
-			if (n % i) == 0:
-				return i
-			else:
-				i += 1
-				continue
-		return 0
+primes = [1, 2, 3]
+numofprimes = 3
+fname = "none"
 
 if len(sys.argv) == 1:
-	p = nextprime()
+	n = 0
 elif len(sys.argv) == 2:
-	p  = nextprime(int(sys.argv[1]))
+	n = int(sys.argv[1])
+elif len(sys.argv) == 3:
+	n = int(sys.argv[1])
+	fname = sys.argv[2]
 else:
 	print "BUH?!"
+
+def nextprime():
+	global primes
+	global numofprimes
+	global fname
+	prime = False
+	n = primes[len(primes) - 1] + 1
+	while True:
+		factor = isprime(n)
+		if factor == 0:
+			primes.append(n)
+			break
+		else:
+			print str(n), "is divisible by", str(factor)
+			n += 1
+	print str(n), "is prime"
+	numofprimes += 1
+def isprime(n):
+	i = 2
+	while i < n:
+		if (n % i) == 0:
+			return i
+		else:
+			i += 1
+			continue
+	return 0
+def outputresults():
+	global primes
+	global numofprimes
+	global fname
+	finalstate = "I found {0}: {1}".format(numofprimes, primes)
+	if fname != "none":
+		with open(fname, 'w') as f:
+			f.write(finalstate)
+	else:
+		print finalstate
+
+if n == 0:
+	while True:
+		nextprime()
+		answer = raw_input("Continue? (Y/n)")
+		if len(answer) > 0 and answer[0].lower() == 'n':
+			outputresults()
+			break
+else:
+	while numofprimes < n:
+		nextprime()
+	outputresults()
