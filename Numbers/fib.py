cffce9e68ef85eabf9b70f90dbc5bd4494f02dd0
Matthew Pherigo <hybrid120@gmail.com>
2014-12-23 20:02:21 -0600

    declassify fib.py

--- Numbers/fib.py
+++ Numbers/fib.py
@@ -2,44 +2,46 @@
 
 import sys
 
-class fib:
-	d = dict()
-	counter = 1
-	def __init__(self, n=0):
-		self.d = {'result': [1, 1], 'iterations': 2}
-		if n == 0:
-			self.printresults()
-			while True:
-				self.iter()
-				self.printlatest()
-				answer = raw_input("Continue? (Y/n) ->").lower()
-				if len(answer) > 0 and answer[0] == 'n':
-					break
-				else:
-					continue
-			print "Done! \n\n"
-			self.printresults()
-		else:
-			self.iter(n - 2)
-			self.printresults()
-	
-	def printresults(self):
-		i = 0
-		while i < len(self.d['result']):
-			print "#" + str(i + 1), "is", str(self.d['result'][i])
-			i += 1
-	def printlatest(self):
-		print "#" + str(self.counter + 1), "is", str(self.d['result'][self.counter])
-	def iter(self, n=1):
-		i = 0
-		while i < n:
-			nextone = self.d['result'][self.counter] + self.d['result'][(self.counter - 1)]
-			self.d['result'].append(nextone)
-			self.d['iterations'] += 1
-			self.counter += 1
-			i += 1
-
 if len(sys.argv) > 1:
-	f = fib(int(sys.argv[1]))
+	n = int(sys.argv[1])
 else:
-	f = fib()
+	n = 0
+
+def printresults():
+	global d
+	i = 0
+	while i < len(d['result']):
+		print "#" + str(i + 1), "is", str(d['result'][i])
+		i += 1
+def printlatest():
+	global counter
+	print "#" + str(counter + 1), "is", str(d['result'][counter])
+def fibiter(n=1):
+	global d
+	global counter
+	i = 0
+	while i < n:
+		nextone = d['result'][counter] + d['result'][(counter - 1)]
+		d['result'].append(nextone)
+		d['fibiterations'] += 1
+		counter += 1
+		i += 1
+
+counter = 1
+d = {'result': [1, 1], 'fibiterations': 2}
+if n == 0:
+	print "#1 is 1\n#1 is 1"
+	while True:
+		fibiter()
+		printlatest()
+		answer = raw_input("Continue? (Y/n) ->").lower()
+		if len(answer) > 0 and answer[0] == 'n':
+			break
+		else:
+			continue
+	print "Done! \n\n"
+	printresults()
+else:
+	fibiter(n - 2)
+	printresults()
+
