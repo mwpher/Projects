5ca5f782124eeccdc5a4e0364253c2fb45a71a7b
Matthew Pherigo <hybrid120@gmail.com>
2015-01-30 15:04:48 -0600

    add coin-flipping sim with arc4random

---
+++ Numbers/coin.c
@@ -0,0 +1,60 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  coin.c
+ *
+ *    Description:  Flip a coin as many times as the user wants
+ *
+ *        Version:  0.1
+ *        Created:  01/30/15 13:27:37
+ *       Revision:  none
+ *       Compiler:  Clang
+ *
+ *         Author:  Matthew Pherigo (mwp), hybrid120@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <limits.h>
+#include <matt.h>
+
+int main(int argc, const char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "%d is the wrong number of arguments!\n", argc);
+        exit(1);
+    }
+
+    unsigned int flips;
+    const unsigned int half = UINT_MAX / 2;
+    const char* errstr;
+    unsigned int i;
+
+    flips = (unsigned int)strtonum(argv[1], 1, UINT_MAX, &errstr);
+
+    if(flips <= 1) {
+        fprintf(stderr, "Number must be an int from 1 to %d. \n strtonum error: %s", UINT_MAX, errstr);
+        exit(EXIT_FAILURE);
+    }
+
+    unsigned int myRandomData[flips];
+    arc4random_buf(myRandomData, sizeof myRandomData);
+
+    for (i = 0; i < flips; i++) {
+        if (myRandomData[i] > half) {
+            printf("Heads! ");
+        }
+        else {
+            printf("Tails! ");
+        }
+        printf(" %u / %u \r\n", myRandomData[i], half);
+    }
+
+    return EXIT_SUCCESS;
+}
